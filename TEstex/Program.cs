3fba126bd200a92cdd35e2414961daf2cc995a53
elpe2005 <epedersen2005@gmail.com>
2022-09-19 10:37:16 +0200

    någonting
    någonting

---
+++ TEstex/Program.cs
@@ -0,0 +1,13 @@
+﻿string[,] spel= new string[,] {{"1","2","3","4"},
+                               {"5","6","7","8"}, 
+                               {"9","10","11","12"},
+                               {"13","14","15","16"}}; 
+
+Console.WriteLine("________________");
+Console.WriteLine("|"+ spel[0,0]+"|"+ spel[0,1]+"|"+ spel[0,2]+"|"+ spel[0,3]+"|");
+Console.WriteLine("|"+ spel[1,0]+"|"+ spel[1,1]+"|"+ spel[1,2]+"|"+ spel[1,3]+"|");
+Console.WriteLine("|"+ spel[2,0]+"|"+ spel[2,1]+"|"+ spel[2,2]+"|"+ spel[2,3]+"|");
+Console.WriteLine("|"+ spel[3,0]+"|"+ spel[3,1]+"|"+ spel[3,2]+"|"+ spel[3,3]+"|");
+
+
+
